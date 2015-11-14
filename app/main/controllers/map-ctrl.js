8a820ac30412b08a651dec1672ae8bc5d81a62f0
Karbon <karbon@pfadipatria.com>
2015-11-14 12:21:46 +0100

    Fix dragging behaviour of map page

--- app/main/controllers/map-ctrl.js
+++ app/main/controllers/map-ctrl.js
@@ -1,8 +1,13 @@
 'use strict';
 angular.module('main')
-.controller('MapCtrl', function ($log, $scope, LocationReporter) {
+.controller('MapCtrl', function ($log, $scope, $state, $ionicSideMenuDelegate, LocationReporter) {
 
   $log.log('Hello from your Controller: MapCtrl in module main:. This is your controller:', this);
+
+  $ionicSideMenuDelegate.canDragContent(false);
+  $scope.$on('$locationChangeSuccess', function( event ) {
+    $ionicSideMenuDelegate.canDragContent($state.current.url !== '/map');
+  });
 
   $scope.center = {
     lat: 0,
