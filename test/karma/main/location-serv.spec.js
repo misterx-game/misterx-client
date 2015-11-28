fb58e464987deb6887371060cb135913f99e84c6
Karbon <karbon@pfadipatria.com>
2015-11-28 01:43:53 +0100

    Test location resource with a mocked backend

--- test/karma/main/location-serv.spec.js
+++ test/karma/main/location-serv.spec.js
@@ -6,14 +6,22 @@ describe('module: main, service: Location', function() {
   beforeEach(module('main'));
   // load all the templates to prevent unexpected $http requests from ui-router
   beforeEach(module('ngHtml2Js'));
+  // load ngResource so we can mock $httpBackend
+  beforeEach(module('ngResource'));
 
-  // instantiate service
-  var Location;
-  beforeEach(inject(function(_Location_) {
+  // instantiate service and it's dependencies
+  var Location, $httpBackend;
+  beforeEach(inject(function(_Location_, _$httpBackend_) {
+    $httpBackend = _$httpBackend_;
     Location = _Location_;
   }));
 
-  it('should build url from Config');
-  it('should create a $resource');
+  it('should build url from Config.ENV', function() {
+    $httpBackend.expectGET('http://localhost:3001/locations')
+      .respond({});
+
+    Location.get();
+    $httpBackend.flush();
+  });
 
 });
