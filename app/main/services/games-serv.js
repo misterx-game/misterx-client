a157131cff4e15536e82b674b70252421d7c1694
mathias petermann <mathias.petermann@unic.com>
2016-01-19 15:03:07 +0100

    enabled single record get for games

--- app/main/services/games-serv.js
+++ app/main/services/games-serv.js
@@ -2,6 +2,6 @@
 angular.module('main')
 .factory('Games', function($resource, Config) {
 
-  return $resource(Config.ENV.SERVER_URL + '/games', {}, {});
+  return $resource(Config.ENV.SERVER_URL + '/games/:gameId', { gameId: '@id' }, {});
 
 });
