7cd69c0fbc8182c3904fd42f1c14aaad3d063bca
mathias petermann <mathias.petermann@unic.com>
2016-01-20 10:18:52 +0100

    removed back link on map view, enabled normal menu

--- app/main/controllers/game-ctrl.js
+++ app/main/controllers/game-ctrl.js
@@ -1,11 +1,14 @@
 'use strict';
 angular.module('main')
- .controller('GameCtrl', function($log, $state, Games) {
+ .controller('GameCtrl', function($log, $state, $ionicHistory, Games) {
 
    // show list of active games
    this.games = Games.query();
 
    this.joinGame = function(gameId) {
+     $ionicHistory.nextViewOptions({
+       historyRoot: true
+     });
      $state.go('main.map', { game: gameId });
    };
 
